73524d68e37a5002e80a7c057c88e4103b6ee67b
AlexeyMonkey13 <Aptuct89@inbox.ru>
2022-12-22 23:16:36 +0700

    beadbbalance

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("введите число " );
+
+int number = Convert.ToInt32(Console.ReadLine());
+int numberb = number + 1;
+if (number % 7 == 0)
+{
+    Console.WriteLine("выходной ");
+}
+if (numberb % 7 == 0)
+{
+    Console.WriteLine("выходной ");
+    
+}
+
+if (number % 7 !=0)
+{
+    Console.WriteLine("Работать !!! ");
+}
+
