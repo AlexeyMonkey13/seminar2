e78872d5f3195ca638a9bf0d2f538bbe591c6e1d
AlexeyMonkey13 <Aptuct89@inbox.ru>
2022-12-22 16:49:19 +0700

    add first task

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// написать программу которая принимает на вход трехзначное число и выводит вторую цифру
+
+Console.WriteLine("введите трехзначное число " );
+
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number;
+int d = 99;
+int c = 1000;
+
+    
+if ( d < a && a < c)
+{
+    Console.WriteLine((a/10) % 10);
+    
+}
+
+else
+{
+    Console.WriteLine("false ");
+}
+
+
+
+//int n = (number / 10) % 10 ;
